646c7850f87be2cc931c5c53aadf50ea39406010
NadiaMok <mokhirevanadezda@gmail.com>
2019-10-12 17:10:31 -0400

    x3

---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent {
+
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
+
+  model = new Hero(18, 'Iron Man', this.powers[0], 'Tony Stark');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+}
